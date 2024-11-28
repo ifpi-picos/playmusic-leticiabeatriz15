d5df725accc3f00bb2080a362c9558dd070811e5
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2024-11-28 17:04:03 -0300

    Enviando arquivos, playMusic com botão play-next-previous

---
+++ src/App.java
@@ -0,0 +1,109 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import dominio.Album;
+import dominio.Artista;
+import dominio.Musica;
+
+public class App {
+     public static void main(String[] args) throws Exception {
+          Musica musica1 = new Musica();
+          musica1.setnomeMusica("Count-Em");
+          musica1.setGenero("Rock");
+          musica1.setDuracao(180);
+          musica1.setArquivoAudio("Basic-Music-App/aula-de-poo-playmusic/assets/Brandon-Lake_Count-Em.wav");
+
+
+          Musica musica2 = new Musica();
+          musica2.setnomeMusica("That's who i praise");
+          musica2.setGenero("Rock");
+          musica2.setDuracao(180);
+          musica2.setArquivoAudio("Basic-Music-App/aula-de-poo-playmusic/assets/Brandon-Lake_Thats-Who-I-Praise.wav");
+
+          Musica musica3 = new Musica();
+          musica3.setnomeMusica("Tear-Off-The-Roof");
+          musica3.setGenero("Rock");
+          musica3.setDuracao(180);
+          musica3.setArquivoAudio("Basic-Music-App/aula-de-poo-playmusic/assets/Brandon-Lake_Tear-Off-The-Roof.wav");
+
+          Album album1 = new Album();
+          album1.setNomeAlbum("Primeiro artista");
+          album1.setAno(2023);
+          album1.addMusica(musica1);
+          album1.addMusica(musica2);
+          album1.addMusica(musica3);
+          
+          Artista BrandonLake = new Artista();
+          BrandonLake.setNome("Brandon Lake");
+          BrandonLake.addAlbum(album1);
+
+          System.out.println("Abrindo PlayMusic...");
+
+          AudioPlayer player = new AudioPlayer();
+
+           // Cria o botão Play/Stop e configura sua ação
+          JButton playStopButton = new JButton("Play");
+          
+          player.listMusic(BrandonLake.getAlbuns().get(0).getMusicas());
+
+          playStopButton.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                    if (!player.isPlaying) {
+                         player.playAudio();
+                         playStopButton.setText("Stop");
+                    }else{
+                         player.stopAudio();
+                         playStopButton.setText("Play");
+                    }
+               }
+          });
+
+          JButton nextAudioButton = new JButton("Next Audio");
+          nextAudioButton.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                    if(player.isPlaying){
+                         player.nextAudio();
+                    }else{
+                         player.nextAudio();
+                         playStopButton.setText("Stop");
+                    }
+               }
+          });
+
+          JButton previousAudioButton = new JButton("Previous Audio");
+          previousAudioButton.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                    if(player.isPlaying){
+                         player.previousAudio();
+                    }else{
+                         player.previousAudio();
+                         playStopButton.setText("Stop");
+                    }
+               }
+          });
+
+
+          ImageIcon icon = new ImageIcon("Basic-Music-App/aula-de-poo-playmusic/assets/imgMusic.png");
+          //Exibe um JOptionPane com o botão Play/Stop
+          JOptionPane.showOptionDialog(
+                null,
+                player,
+                "PlayMusic",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                icon,
+                new Object[] { previousAudioButton, playStopButton, nextAudioButton }, playStopButton);
+
+          // Fecha o clip de áudio ao encerrar o programa
+          if (player.audioClip != null) {
+               player.audioClip.close();
+          }
+     }
+}
---
+++ src/AudioPlayer.java
@@ -0,0 +1,120 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import dominio.Musica;
+
+public class AudioPlayer {
+    private List<Musica> listaDeReproducao;
+    private int indice;
+    public Clip audioClip;
+    public boolean isPlaying = false;
+
+  
+    public void loadAudio(String filePath) {
+      System.out.println("loadAudio: " + filePath);
+      try {
+
+        // Se um áudio já estiver carregado, pare e libere o recurso
+        if (audioClip != null && audioClip.isOpen()) {
+          audioClip.stop();
+          audioClip.close();
+        }
+
+        // Carrega o arquivo de áudio
+        File audioFile = new File(filePath);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+  
+        // Configura o Clip
+        AudioFormat format = audioStream.getFormat();
+        DataLine.Info info = new DataLine.Info(Clip.class, format);
+        audioClip = (Clip) AudioSystem.getLine(info);
+  
+        audioClip.open(audioStream);
+      } catch (UnsupportedAudioFileException e) {
+        System.out.println("O formato do arquivo de áudio não é suportado.");
+        e.printStackTrace();
+      } catch (LineUnavailableException e) {
+        System.out.println("A linha de áudio não está disponível.");
+        e.printStackTrace();
+      } catch (IOException e) {
+        System.out.println("Erro ao ler o arquivo de áudio.");
+        e.printStackTrace();
+      }
+    }
+
+    public void listMusic(List<Musica> musicas){
+      this.listaDeReproducao = musicas;
+      this.indice = 0;
+      loadAudio(musicas.get(indice).getArquivoAudio());
+    }
+  
+    public void playAudio() {
+      System.out.println("playAudio");
+      
+      if (audioClip != null && !isPlaying) {
+        audioClip.setFramePosition(0); // Reinicia o áudio do começo
+        System.out.println("playAudio start");
+        audioClip.start();
+        isPlaying = true;
+      }
+    }
+  
+    public void stopAudio() {
+      System.out.println("stopAudio");
+
+      if (audioClip != null && isPlaying) {
+        System.out.println("stopAudio stop");
+        audioClip.stop();
+        isPlaying = false;
+      }
+    }
+
+    public void previousAudio (){
+
+      System.out.println("previousAudio");
+      stopAudio();
+
+      if(this.indice == 0){
+        indice = listaDeReproducao.size() - 1;
+        loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
+        playAudio();
+
+      }else if(this.indice <= listaDeReproducao.size() - 1){
+        indice --;
+        loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
+        playAudio();
+
+      }
+    }
+
+    public void nextAudio (){
+
+      System.out.println("nextAudio");
+      stopAudio();
+
+      if(this.indice < listaDeReproducao.size() - 1){
+        indice ++;
+        loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
+        playAudio();
+
+      }else if(this.indice == listaDeReproducao.size() - 1){
+        indice = 0;
+        loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
+        playAudio();
+
+      }else{
+        System.out.println("Sua lista de reprodução está vazia!\nAdicione músicas para tocar...");
+
+      }
+    }
+
+  }  
